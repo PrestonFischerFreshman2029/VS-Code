c667721548842e9603797fb871eac44bbdc378bd
Preston Fischer <fischpre000@rocori.k12.mn.usgit config --global user.name PrestonFischerFreshman2029git config --global user.email fischpre000@rocori.k12.mn.us>
2025-11-06 13:22:27 -0600

    done

---
+++ Slicing decoder/Slicing Decoder.py
@@ -0,0 +1,16 @@
+msg_a = "14#9&!!#'%7'&%This is Python12!@#%$&¨*(387"
+
+decoded_a = msg_a[14:28]
+print("Decoded message (a):", decoded_a)
+
+
+msg_b = "!$Y@@o* u%$ #uaarreeqe$  raa@m%$a!dz¨zidmn!ag#g!"
+
+decoded_b = msg_b[2::3]
+print("Decoded message (b):", decoded_b)
+
+
+msg_c = "3!$2%¨@* &6(8?ti teg uoy diDlask"
+
+decoded_c = msg_c[26:11:-1]
+print("Decoded message (c):", decoded_c)
