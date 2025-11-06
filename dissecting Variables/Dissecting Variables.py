4f7716e7aa9e0de95f8ad1ecb0a43c694d0114f1
Preston Fischer <fischpre000@rocori.k12.mn.usgit config --global user.name PrestonFischerFreshman2029git config --global user.email fischpre000@rocori.k12.mn.us>
2025-11-06 13:24:03 -0600

    done

---
+++ dissecting Variables/Dissecting Variables.py
@@ -0,0 +1,10 @@
+
+user_input = input("Type in something: ")
+
+print("\nAre there only spaces?", user_input.isspace())
+print("Is it a number?", user_input.isdigit())
+print("Is it alphabetical?", user_input.isalpha())
+print("Is it alphanumeric?", user_input.isalnum())
+print("Is it all uppercase?", user_input.isupper())
+print("Is it all lower case?", user_input.islower())
+print("Is it capital case?", user_input.istitle())
